2500b9588f29bc7445c76468a4bdcd246c6831ab
alexhwoods <alex@makeswift.com>
2024-04-13 23:26:22 -0400

    todo: add note for add

--- src/commands/add.ts
+++ src/commands/add.ts
@@ -1,6 +1,13 @@
-import fs from "fs";
-import path from "path";
-import { writeTree } from "./write-tree";
-import { commitTree } from "./commit-tree";
-
-export function add({ root, paths }: { root: string; paths: string[] }) {}
+export function add({ root, paths }: { root: string; paths: string[] }) {
+  // @todo: so, editing the index file is not so easy eh
+  /**
+   * 1. For each path:
+   *  a. Call hash-object - this will create the object if it doesn't exist
+   *  b. Get the SHA-1 hash
+   *  c. Write the entry to the index file
+   *
+   * It's this step 1c that's a pain, because the index file is a binary file.
+   *
+   * To peer into it's complexities, see the ls-files.ts file.
+   */
+}
